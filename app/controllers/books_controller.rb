bb4fd12fa0d6d6e6e1e45f848711a49c16bbe019
iriehayato <143862129+iriehayato12@users.noreply.github.com>
2023-09-09 09:15:57 +0000

    rails2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,47 @@
 class BooksController < ApplicationController
+  def new
+  @book = Book.new
+  end
 
-    
+  def index
+  @books = Book.all
+  end
+
+  def edit
+  @book = Book.find(params[:id])
+  end
+
+  def show
+  @book = Book.find(params[:id])
+  end
+  
+  def create
+  @book = Book.new(book_params)
+  if @book.save
+  flash[:notice] = "Book was successfully created!"
+  redirect_to book_path(book.id)
+  end
+  end
+  
+  def update
+  book = Book.find(params[:id])
+  if book.update(book_params)
+  flash[:notice] = "Book was successfully updated!"
+  redirect_to book_path(book.id)
+  end
+  end
+  
+  def destroy
+  book = Book.find(params[:id])
+  if book.destroy
+  flash[:notice] = "Book was successfully destroyed"
+  redirect_to '/books'
+  end
+  end
+  
+  
+  private
+  def book_params
+  params.require(:book).permit(:title,:body)
+  end
 end
